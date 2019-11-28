ab26ba4c25365c6cd3f2bbe50035254b1734c112
AlexShom <a.shomalistos@gmail.com>
2019-11-28 14:52:43 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (array) => {
+  let result = array.find(el => el.result === "W")
+  return result ? result.year : undefined
+}
